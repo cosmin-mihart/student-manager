6d0d093d48090be103b2e645a8ba60890bd2351f
Cosmin Mihart <mihartcosmin80@gmail.com>
2023-03-23 17:15:03 +0200

    SM-1 Added the login screen

---
+++ src/sample/Main.java
@@ -0,0 +1,7 @@
+@Override
+public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
+        primaryStage.setTitle("FIS - Student Manager");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+        }
